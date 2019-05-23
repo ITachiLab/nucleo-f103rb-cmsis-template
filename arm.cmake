fc7509b518ef4ea3bd515295cb4f85854cc553de
Bąk, Krzysztof <krzysztof.bak@cirrus.com>
2019-05-23 02:25:43 +0200

    A small improvement in toolchain file

--- arm.cmake
+++ arm.cmake
@@ -2,16 +2,24 @@ set(CMAKE_SYSTEM_NAME "Generic")
 set(CMAKE_SYSTEM_PROCESSOR "arm")
 
 # Adapt this to a valid toolchain location
+if (UNIX)
+set(tools "/opt/gcc-arm-none-eabi-6-2017-q2-update")
+set(suffix "")
+endif (UNIX)
+
+if (WIN32)
 set(tools "C:/opt/gcc-arm-none-eabi-6-2017-q2-update-win32")
+set(suffix ".exe")
+endif (WIN32)
 
 set(prefix "arm-none-eabi")
 set(compiler_flags "--specs=nosys.specs -Wall -fdata-sections -ffunction-sections")
 set(mcu "-mcpu=cortex-m3 -mthumb")
 
-set(CMAKE_C_COMPILER "${tools}/bin/${prefix}-gcc.exe")
-set(CMAKE_C_COMPILER_AR "${tools}/bin/${prefix}-ar.exe")
-set(CMAKE_ASM_COMPILER "${tools}/bin/${prefix}-gcc.exe")
-set(CMAKE_LINKER "${tools}/bin/${prefix}-ld.exe")
+set(CMAKE_C_COMPILER "${tools}/bin/${prefix}-gcc${suffix}")
+set(CMAKE_C_COMPILER_AR "${tools}/bin/${prefix}-ar${suffix}")
+set(CMAKE_ASM_COMPILER "${tools}/bin/${prefix}-gcc${suffix}")
+set(CMAKE_LINKER "${tools}/bin/${prefix}-ld${suffix}")
 
 set(CMAKE_C_FLAGS "${mcu} ${compiler_flags}")
 set(CMAKE_ASM_FLAGS "${mcu} ${compiler_flags}")
