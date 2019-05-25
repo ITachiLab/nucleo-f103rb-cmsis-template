ceb43704fa590134e2a46fca0b9dddf75ec2b0c4
Krzysztof Bąk <contact@itachi.pl>
2019-05-25 22:51:38 +0200

    Improved toolchain file; moved mandatory libraries to toolchaon file.

--- arm.cmake
+++ arm.cmake
@@ -13,8 +13,10 @@ set(suffix ".exe")
 endif (WIN32)
 
 set(prefix "arm-none-eabi")
-set(compiler_flags "--specs=nosys.specs -Wall -fdata-sections -ffunction-sections")
+set(compiler_flags "-Wall -fdata-sections -ffunction-sections -Os")
+set(linker_script "${CMAKE_SOURCE_DIR}/platform/STM32F103XB_FLASH.ld")
 set(mcu "-mcpu=cortex-m3 -mthumb")
+set(linker_flags "${mcu} -specs=nano.specs -T${linker_script} -lc -lm -lnosys -Wl,--gc-sections")
 
 set(CMAKE_C_COMPILER "${tools}/bin/${prefix}-gcc${suffix}")
 set(CMAKE_C_COMPILER_AR "${tools}/bin/${prefix}-ar${suffix}")
@@ -23,4 +25,4 @@ set(CMAKE_LINKER "${tools}/bin/${prefix}-ld${suffix}")
 
 set(CMAKE_C_FLAGS "${mcu} ${compiler_flags}")
 set(CMAKE_ASM_FLAGS "${mcu} ${compiler_flags}")
-set(CMAKE_EXE_LINKER_FLAGS "${mcu} -T ${CMAKE_SOURCE_DIR}/platform/STM32F103XB_FLASH.ld")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${linker_flags}") 
