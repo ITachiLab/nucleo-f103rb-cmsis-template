9a6f11908b64c9efc6d28dfc32c0f3c515b9a1bc
Krzysztof Bąk <contact@itachi.pl>
2019-05-28 02:03:44 +0200

    Enabled building bin and hex files Improvements in toolchain file

--- arm.cmake
+++ arm.cmake
@@ -13,16 +13,19 @@ set(suffix ".exe")
 endif (WIN32)
 
 set(prefix "arm-none-eabi")
-set(compiler_flags "-Wall -fdata-sections -ffunction-sections -Os")
+set(compiler_flags "-DSTM32F103xB -Wall -fdata-sections -ffunction-sections -Os")
 set(linker_script "${CMAKE_SOURCE_DIR}/platform/STM32F103XB_FLASH.ld")
 set(mcu "-mcpu=cortex-m3 -mthumb")
-set(linker_flags "${mcu} -specs=nano.specs -T${linker_script} -lc -lm -lnosys -Wl,--gc-sections")
+set(linker_flags "-specs=nano.specs -T${linker_script} -lc -lm -lnosys -Wl,--gc-sections")
 
 set(CMAKE_C_COMPILER "${tools}/bin/${prefix}-gcc${suffix}")
 set(CMAKE_C_COMPILER_AR "${tools}/bin/${prefix}-ar${suffix}")
 set(CMAKE_ASM_COMPILER "${tools}/bin/${prefix}-gcc${suffix}")
 set(CMAKE_LINKER "${tools}/bin/${prefix}-ld${suffix}")
 
+set(OBJCOPY "${tools}/bin/${prefix}-objcopy${suffix}")
+set(SIZE "${tools}/bin/${prefix}-size${suffix}")
+
 set(CMAKE_C_FLAGS "${mcu} ${compiler_flags}")
 set(CMAKE_ASM_FLAGS "${mcu} ${compiler_flags}")
-set(CMAKE_EXE_LINKER_FLAGS "${linker_flags}") 
+set(CMAKE_EXE_LINKER_FLAGS "${mcu} ${linker_flags}") 
